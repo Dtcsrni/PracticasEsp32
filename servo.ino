4ddc6e3bc9af11855bab77cb5cc1fcf304e4db03
Erick Vega <armsystechno@gmail.com>
2023-08-25 20:21:36 -0600

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -5,19 +5,12 @@ Servo servo;
 int pinPotenciometro=2;
 int pinServo=4;
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(pinPotenciometro, INPUT);
   servo.attach(pinServo, 500, 2500);
 }
 
 void loop() {
-  entradaPotenciometro = analogRead(pinPotenciometro);
-  anguloServo = (entradaPotenciometro * 0.04395604395);
-  Serial.print("entradaPotenciometro:");
-  Serial.println(entradaPotenciometro);
-  Serial.print("anguloServo:");
-  Serial.println(anguloServo);
-  Serial.println("------------------");
-  servo.write(anguloServo);
-  delay(10); 
+  servo.write(map(analogRead(pinPotenciometro), 0, 4095, 0, 190));
+  delay(3000); 
 }
